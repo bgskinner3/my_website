038de2b8023d3ee63f9dfc2f09260f499b853dd4
Brennan Skinner <bgskinner3@gmail.com>
2023-04-03 00:14:41 -0400

    formmatting mobile

--- src/components/MainMobile/Banner.js
+++ src/components/MainMobile/Banner.js
@@ -7,18 +7,22 @@ const Banner2 = () => {
 
   return (
     <div className="container mb-52">
-      <img src={Mobile} alt="" className="absolute object-cover w-screen h-screen brightness-50" />
-      <div className="box">
-        <div class="title ">
+      <img
+        src={Mobile}
+        alt=""
+        className="absolute object-cover w-screen h-screen brightness-50"
+      />
+      <div className="box ">
+        <div className="title ">
           <span className="block"></span>
-          <h1 className="">
+          <h1 className="justify-center">
             Brennan Skinner<span></span>
           </h1>
         </div>
 
-        <div className="role">
+        <div className="role ">
           <div className="block"></div>
-         
+
           <p className="pr-5 text-red-600">FULL-STACK</p>
           <p className="text-white">DEVELOPER</p>
         </div>
